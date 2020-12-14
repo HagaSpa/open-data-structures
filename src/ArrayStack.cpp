e7a030b8669900ba24f4cccd0a187c482830d753
HagaSpa <justicesparrow13@gmail.com>
2020-12-14 10:36:57 +0900

    implemented ArrayStack

---
+++ src/ArrayStack.cpp
@@ -0,0 +1,44 @@
+#include "array.cpp"
+
+template<class T>
+class ArrayStack {
+private:
+    array<T> a;
+    int n;
+    int size();
+public:
+    ArrayStack();
+    T get(int i);
+    T set(int i, T x);
+};
+
+template<class T> inline
+int ArrayStack<T>::size() {
+    return n;
+}
+
+template<class T> inline
+T ArrayStack<T>::get(int i) {
+    return a[i];
+}
+
+template<class T> inline
+T ArrayStack<T>::set(int i, T x) {
+    T y = a[i];
+    a[i] = x;
+    return y;
+}
+
+template<class T>
+ArrayStack<T>::ArrayStack() : a(1) {
+    n = 0;		
+}
+
+
+// entry point
+#include<iostream>
+using namespace std;
+int main() {
+    ArrayStack<int> l;
+    cout << l.get(0) << endl;
+}
